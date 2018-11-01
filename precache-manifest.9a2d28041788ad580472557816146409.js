e80cca2a13dd14f91fef4933bae2aa740a512c76
Annar <ahi046@student.uib.no>
2018-11-01 12:34:41 +0100

    Updates

---
+++ precache-manifest.9a2d28041788ad580472557816146409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kvarteret_infopanel/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "542307082f3837270f84",
+    "url": "/kvarteret_infopanel/static/js/runtime~main.54230708.js"
+  },
+  {
+    "revision": "7db71299a910b0cf4d46",
+    "url": "/kvarteret_infopanel/static/js/main.7db71299.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/kvarteret_infopanel/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "7db71299a910b0cf4d46",
+    "url": "/kvarteret_infopanel/static/css/main.2b09b31c.chunk.css"
+  },
+  {
+    "revision": "a0feff2287dac8a6e21ae201daa758cb",
+    "url": "/kvarteret_infopanel/index.html"
+  }
+];
